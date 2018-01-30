18069b50eee8a7170a03ce0d7ed9d3b430b15df8
BenjiEng <benjamin.eng7@gmail.com>
2018-01-30 00:32:23 -0800

    to ES6

--- app/assets/javascripts/components/appointments.es6.jsx
+++ app/assets/javascripts/components/appointments.es6.jsx
@@ -1,44 +1,52 @@
-var Appointments = React.createClass({
-  getInitialState: function() {
-    return {
+class Appointments extends React.Component{
+
+  constructor(props) {
+    super(props) //calls the constructor of the parent class
+    this.state = {
       appointments: this.props.appointments,
       title: '',
       appt_time: ''
     }
-  },
+  }
 
-  handleUserInput: function(obj) {
+  handleUserInput(obj) {
     this.setState(obj);
-  },
+  }
 
-  handleFormSubmit: function() {
+  handleFormSubmit() {
     var appointment = {title: this.state.title, appt_time: this.state.appt_time};
     $.post('/appointments',
             {appointment: appointment})
           .done(function(data) {
             this.addNewAppointment(data);
           }.bind(this));
-  },
+  }
 
-  addNewAppointment: function(appointment) {
+  addNewAppointment(appointment) {
     var appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
     this.setState({
       appointments: appointments.sort(function(a,b){
         return new Date(a.appt_time) - new Date(b.appt_time);
       })
     });
-  },
+  }
 
-  render: function() {
+  handleDeleteAppt(id) {
+    debugger
+    alert('delete fired');
+  }
+
+  render() {
     return (
       <div>
         <AppointmentForm input_title={this.state.title}
           input_appt_time={this.state.appt_time}
-          onUserInput={this.handleUserInput}
-          onFormSubmit={this.handleFormSubmit} />
+          onUserInput={this.handleUserInput.bind(this)}
+          onFormSubmit={this.handleFormSubmit.bind(this)} />
         <h2>Your Appointments</h2>
-        <AppointmentsList appointments={this.state.appointments} />
+        <AppointmentsList appointments={this.state.appointments} 
+                          onApptDelete={this.handleDeleteAppt} />
       </div>
     )
   }
-});
+}
